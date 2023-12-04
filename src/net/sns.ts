504a10e43b56ede193a64d43af2ae6b88eb9f879
Xisco Sastre Cabot <francesc.sastrecabot@showpad.com>
2023-12-04 23:23:14 +0100

    add apns attributes

--- src/net/sns.ts
+++ src/net/sns.ts
@@ -2,17 +2,22 @@ import { PublishCommand, PublishCommandInput, SNSClient } from '@aws-sdk/client-
 
 export async function publishToSns(topic: string, payload: string) {
   const snsClient = new SNSClient({})
-  const dataObj = { data: JSON.parse(payload) }
+  const object = JSON.parse(payload)
   const pushNotificationPayload = {
     default: payload,
-    GCM: JSON.stringify(dataObj),
-    APNS: JSON.stringify({ aps: { 'content-available': 1 }, ...dataObj }),
+    GCM: JSON.stringify({ data: object }),
+    APNS: JSON.stringify({ aps: { 'content-available': 1 }, data: object }),
   }
 
   const notificationInput: PublishCommandInput = {
     Message: JSON.stringify(pushNotificationPayload),
     TopicArn: topic,
     MessageStructure: 'json',
+    MessageAttributes: {
+      'AWS.SNS.MOBILE.APNS.TOPIC': { DataType: 'string', StringValue: 'com.apploading.bnmallorca' },
+      'AWS.SNS.MOBILE.APNS.PUSH_TYPE': { DataType: 'string', StringValue: 'background' },
+      'AWS.SNS.MOBILE.APNS.PRIORITY': { DataType: 'string', StringValue: '5' },
+    },
   }
 
   await snsClient.send(new PublishCommand(notificationInput))
