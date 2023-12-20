3d5f3a272a42a853f67b03ea4fb6ccebc734a912
Xisco Sastre Cabot <francesc.sastrecabot@showpad.com>
2023-12-20 18:37:01 +0100

    change logging

--- src/lambda/delete-devices.lambda.ts
+++ src/lambda/delete-devices.lambda.ts
@@ -3,8 +3,8 @@ import { removeDisabledDevices } from '../service/device.service'
 
 export async function handler(event: any): Promise<any> {
   try {
-    if (event !== undefined) log.info(JSON.stringify(event))
     await removeDisabledDevices()
+    if (event !== undefined) log.info(JSON.stringify(event))
   } catch (e: any) {
     log.error('error deleting devices', e)
   }
