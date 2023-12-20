848e50677217e734f1df28d0499fcda95707b3ce
Xisco Sastre Cabot <francesc.sastrecabot@showpad.com>
2023-12-20 18:22:18 +0100

    add try catch

--- src/lambda/delete-devices.lambda.ts
+++ src/lambda/delete-devices.lambda.ts
@@ -2,6 +2,10 @@ import * as log from 'lambda-log'
 import { removeDisabledDevices } from '../service/device.service'
 
 export async function handler(event: any): Promise<any> {
-  log.info(JSON.stringify(event))
-  await removeDisabledDevices()
+  try {
+    if (event !== undefined) log.info(JSON.stringify(event))
+    await removeDisabledDevices()
+  } catch (e: any) {
+    log.error('error deleting devices', e)
+  }
 }
