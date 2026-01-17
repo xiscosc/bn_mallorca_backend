54245bb326b2ba64407279375f502f5226726849
Xisco Sastre Cabot <xiscosastre@gmail.com>
2026-01-17 12:44:47 +0100

    Update VSCode launch configuration for Bun and enhance logging in track processing lambda

--- src/function/track/poll-new-track.lambda.ts
+++ src/function/track/poll-new-track.lambda.ts
@@ -5,15 +5,17 @@ import { TrackService } from '../../service/track.service';
 
 export async function handler(event: SQSEvent): Promise<void> {
   try {
-    log.info(event?.Records[0]?.messageId ?? '');
+    log.info(event?.Records?.[0]?.messageId ?? 'no-valid-message-id');
     const track = await TrackService.getCurrentTrack();
     if (track === undefined || !stringIsValid(track.artist) || !stringIsValid(track.name)) {
       log.error(`Process track: Artist or name are invalid - ${JSON.stringify(track)}`);
       return;
     }
 
+    log.info(`Track: ${JSON.stringify(track)}`);
     const trackService = new TrackService();
     const hasChanged = await trackService.trackHasChanged(track);
+    log.info(`Has changed: ${hasChanged}`);
     if (hasChanged) await trackService.processTrack(track);
   } catch (err: unknown) {
     const errorMessage = err instanceof Error ? err.message : String(err);
